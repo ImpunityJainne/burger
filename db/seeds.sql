35e7c3a7e17ed485c478689b733f5208eee6c619
Mandie Kramer <37544463+ImpunityJainne@users.noreply.github.com>
2018-09-07 19:12:48 -0500

    did all db and config instructions, save writing orm functions

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured)
+VALUES ("A plain burger, just a bun and the patty. Dry.", false),
+	   ("Hoity Toity Burger, with ostrich patty, arugula, and grilled fig halves.", true),
+	   ("A curry burger, with tumeric-chicken patty, and green chutney.", true);
+
+SELECT * from burgers;
