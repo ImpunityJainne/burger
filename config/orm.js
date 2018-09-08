56362fb04f7fa2c220d60f518d00ddd14d7884d3
Mandie Kramer <37544463+ImpunityJainne@users.noreply.github.com>
2018-09-08 17:51:33 -0500

    added var connection to top of connection.js, maybe that's what's broken?

--- config/orm.js
+++ config/orm.js
@@ -14,7 +14,7 @@ function objToSql(ob) {
   var arr = [];
 
   for (var key in ob) {
-    var value - ob[key];
+    var value = ob[key];
 
     if (Object.hasOwnProperty.call(ob, key)) {
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
